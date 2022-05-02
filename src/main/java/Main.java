6154c398026dd4359dba6f93ef0e56fe92053eed
CarstenO <carsteno@uia.no>
2022-05-02 14:35:33 +0200

    jæ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import main.java.wav.extract.data.WavToRawDouble;
 
 import java.util.List;
 
-
 /*
 *
 *   This program is meant to display graphics on projectors
@@ -23,9 +22,41 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        int w1 = 100;
+        
+        Dog animal = new Dog(w1, "Pluto");
+        Dog animal2 = new Dog(w1, "Pluto");
+        Animal animal3 = new Cat(w1, "Findus");
+        Animal ani2 = animal2;
+
+
+        System.out.println("is ani2 the same as animals2? " + ani2.equals(animal2));
+        animal.getWeight();
+        animal.makeSound();
+
+        System.out.println("Are they the same? " + animal.equals(animal2));
+
+        System.out.println("Animal 1 hashcode = " + animal.hashCode());
+        System.out.println("Animal 2 hashcode = " + animal2.hashCode());
+
+
+        System.out.println("Animal 3 is heavy af " + animal3.getWeight() + " kg ");
+
+
+
+
+
+
+*/
+        System.out.println("Lenght is " + WavToRawDouble.getAudioData("./music/AnimalsRemix.wav").length);
+        //double[] buff = WavToRawDouble.getAudioData("./music/AnimalsRemix.wav");
+        System.out.println("\n");
+
         List soundData = WavSlowToRawDouble.getAudioData("./music/AnimalsRemix.wav");
         System.out.println("Lenght is " + soundData.size());
-        System.out.println("\n\n\n");
-        System.out.println("Lenght is " + WavToRawDouble.getAudioData("./music/AnimalsRemix.wav").length);
+
+
+
     }
 }
